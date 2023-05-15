2edde8a3cfde8cb2f4cfe7cb63c8edfa968c49af
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-15 13:52:55 +0200

    Update tiempo.py

--- tiempo.py
+++ tiempo.py
@@ -6,3 +6,13 @@ def tiempo():
             "units":"metric",
             "APPID":""}
   respuesta=requests.get("http://api.openweathermap.org/data/2.5/weather",params=parametros)
+  if respuesta.status_code == 200:
+    # La respuesta json se convierte en un diccionario
+    datos = respuesta.json()
+    # Se obtienen los valores del diccionario
+    print("La temperatura actual es:",datos["main"]["temp"],"ºC")
+    print("La sensación térmica es:",datos["main"]["feels_like"],"ºC")
+    print("La temperatura mínima es:",datos["main"]["temp_min"],"ºC")
+    print("La temperatura máxima es:",datos["main"]["temp_max"],"ºC")
+    print("La presión es:",datos["main"]["pressure"],"hPa")
+    print("La humedad es:",datos["main"]["humidity"],"%")
